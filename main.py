639c5f628e6872f99151f3fdd2e656749eb54ec2
Le Duc Duy <41518283+leduy8@users.noreply.github.com>
2021-04-05 13:48:31 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+
+"""
+Bài 1: Viết chương trình tìm các số nguyên tố trong một dãy số.
+Exercise 1: Write a program to find the prime numbers within a range of numbers.
+
+Sample input: start = 3, end = 6
+Sample output: 3
+6
+
+Sample input: start = 0, end = 4
+Sample output: 3
+"""
+
+
+def main(start, end):
+    pass
+
+
+main(3, 6)
